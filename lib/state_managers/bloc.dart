9c038531b08552b2a06a44002a928e279cca432b
sgc93 <smachewgedefaw@gmail.com>
2023-10-14 12:36:24 -0900

    add validator StreamTransformers

--- lib/state_managers/bloc.dart
+++ lib/state_managers/bloc.dart
@@ -1,6 +1,18 @@
 import 'dart:async';
 
-class Bloc {
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
+import 'package:bloc_patter/state_managers/transformer.dart';
+
+class Blo with Transformer {
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  // change data
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  // add data
+  Stream<String> get emailStream =>
+      _emailController.stream.transform(emailValidator);
+  Stream<String> get passwordStream =>
+      _passwordController.stream.transform(passwordValidator);
 }
