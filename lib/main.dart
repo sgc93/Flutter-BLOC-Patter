ccaea6b2d552a694c2a950a15e421ff8a581fc0b
sgc93 <smachewgedefaw@gmail.com>
2023-10-16 04:20:28 -0900

    wrap MaterialApp widget with Provider widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_patter/pages/login_page.dart';
+import 'package:bloc_patter/state_managers/provider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Login Page with BLOC patter',
-      home: LoginPage(),
+    return Provider(
+      child: const MaterialApp(
+        title: 'Login Page with BLOC patter',
+        home: LoginPage(),
+      ),
     );
   }
 }
