efd8ad170799a29f683b80eb2a489a5417df3eee
sgc93 <smachewgedefaw@gmail.com>
2023-10-16 04:38:53 -0900

    refactor Provider widget

--- lib/state_managers/provider.dart
+++ lib/state_managers/provider.dart
@@ -2,13 +2,15 @@ import 'package:bloc_patter/state_managers/bloc.dart';
 import 'package:flutter/material.dart';
 
 class Provider extends InheritedWidget {
-  Provider({super.key, required super.child});
-
   final bloc = Bloc();
-  @override
-  bool updateShouldNotify(oldWidget) => true;
 
-  static Bloc of(context) {
-    return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
+  Provider({Key? key, required Widget child}) : super(key: key, child: child);
+
+  static Bloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<Provider>() as Provider)
+        .bloc;
   }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 }
